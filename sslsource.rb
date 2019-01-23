579fbf59a037f231a24b94c3ed1aa18c7b4fe27b
David Anderson <dave@dubkat.com>
2019-01-23 12:22:35 -0700

    Updates formula class name

--- sslsource.rb
+++ sslsource.rb
@@ -1,4 +1,4 @@
-class sslsource < Formula
+class SSLSource < Formula
   desc "The agent for https://sslsource.io"
   homepage "https://sslsource.io"
   url "http://archive.sslsource.io/darwin/sslsource_1.0.1-darwin_amd64.tgz"
