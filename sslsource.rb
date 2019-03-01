8f13c57406f3ac6f336a407a63f547085ed39b82
Dave Anderson <dave@dubkat.com>
2019-02-28 20:37:26 -0700

    Update to 1.0.5

--- sslsource.rb
+++ sslsource.rb
@@ -1,12 +1,12 @@
 class Sslsource < Formula
   desc "The agent for https://sslsource.io"
   homepage "https://sslsource.io"
-  url "http://archive.sslsource.io/darwin/sslsource_1.0.1-darwin_amd64.tgz"
-  sha256 "c5670a086ba9a256cf0be2460609a8c5cf86fe2d58c67d75acb0b7bf4a714760"
+  url "http://archive.sslsource.io/darwin/sslsource_1.0.5-darwin_x86_64.tgz"
+  sha256 "60e548e28f43a3adc99ea8b834d757fc73191b52b28fbf32986f046ee2f1222e"
 
   bottle :unneeded
 
   def install
-    bin.install "usr/bin/sslsource"
+    bin.install "bin/sslsource"
   end
 end
